74068e3746a82bc9efe03de05f7182b0960b3a66
galetilakshmi-0110 <galetilakshmipriyanka0110@gmail.com>
2025-08-14 06:20:50 +0530

    merge conflict resolved

--- frontend/hooks/useTamperCheck.ts
+++ frontend/hooks/useTamperCheck.ts
@@ -1,15 +1,15 @@
-// hooks/useTamperCheck.ts
-import { useEffect } from "react";
-import { getHash } from "../utils/hash";
+// // hooks/useTamperCheck.ts
+// import { useEffect } from "react";
+// import { getHash } from "../utils/hash";
 
-export const useTamperCheck = () => {
-  useEffect(() => {
-    const currentHash = getHash();
+// export const useTamperCheck = () => {
+//   useEffect(() => {
+//     const currentHash = getHash();
 
-    // Only alert if tampering detected
-    const EXPECTED_HASH = localStorage.getItem("expectedHash"); // store the original hash once
-    if (EXPECTED_HASH && currentHash !== EXPECTED_HASH) {
-      alert("⚠️ App integrity check failed! Possible tampering detected.");
-    }
-  }, []);
-};
+//     // Only alert if tampering detected
+//     const EXPECTED_HASH = localStorage.getItem("expectedHash"); // store the original hash once
+//     if (EXPECTED_HASH && currentHash !== EXPECTED_HASH) {
+//       alert("⚠️ App integrity check failed! Possible tampering detected.");
+//     }
+//   }, []);
+// };
